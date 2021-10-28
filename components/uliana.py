a73e53035e2feacdf96e5b74ac2d4fd51b82d90d
Your Name <you@example.com>
2021-10-28 17:39:35 +0500

    create main.py file

--- components/uliana.py
+++ components/uliana.py
@@ -1,3 +1,3 @@
 import turtle
 
-def elohin():
+def uliana():
---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+
+from components.DimaPulman import DimaPulman
+from components.imp import imp
+from components.potapov import potapov
+from components.psareva import psareva
+from components.salmon import salmon
+from components.uliana import uliana
+
+DimaPulman()
+imp()
+potapov()
+psareva()
+salmon()
+uliana()
